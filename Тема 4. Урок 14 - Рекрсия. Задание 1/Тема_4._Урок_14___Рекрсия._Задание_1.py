7d1e5f1826b3233e77361d3c1f5047a5de69904c
79997 <79997@CYBERPC>
2023-05-19 14:22:25 +0400

    Добавьте файлы проекта.

---
+++ Тема 4. Урок 14 - Рекрсия. Задание 1/Тема_4._Урок_14___Рекрсия._Задание_1.py
@@ -0,0 +1,11 @@
+﻿my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+
+def recurs_print(my_list):
+    if my_list == []:
+        print('Конец списка.')
+    else:
+        print(my_list.pop(0)) 
+        recurs_print(my_list)
+
+recurs_print(my_list)
